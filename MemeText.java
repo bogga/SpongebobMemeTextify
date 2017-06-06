05367b8ed1a3987720bbf4a986e8310ee831014b
Adam Bogg <bogga@users.noreply.github.com>
2017-06-06 17:42:24 +0100

    Initial Commit

---
+++ MemeText.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class MemeText
+{
+	private String text;
+
+	public MemeText(String s)
+	{
+		String temp = "";
+		char tempChar;
+		Random rand = new Random();
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (rand.nextInt(5) == 0)
+			{
+				tempChar = s.charAt(i);
+				tempChar = Character.toUpperCase(tempChar);
+			}
+			else
+			{
+				tempChar = s.charAt(i);
+				tempChar = Character.toLowerCase(tempChar);
+			}
+
+			temp += tempChar;
+		}
+
+		text = temp;
+	}
+
+	public String getText()
+	{
+		return text;
+	}
+
+	public static void main(String[] args)
+	{
+		MemeText mt = new MemeText(args[0]);
+		System.out.println(mt.getText());
+	}
+}
